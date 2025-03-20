2801056147a66e693cf730d32564a46814776466
LimitToInfinity <21stLimitless@gmail.com>
2025-03-19 20:02:37 -0600

    Validations for title and price, and refactor controller

--- app/controllers/things_controller.rb
+++ app/controllers/things_controller.rb
@@ -1,26 +1,19 @@
 class ThingsController < ApplicationController
-  before_action :set_thing, only: %i[ show update destroy ]
+  before_action :set_thing, only: %i[show update destroy]
 
   # GET /things
   def index
-    things_data = Thing.all.index_by(&:id).transform_values do |thing|
-      {
-        id: thing.id,
-        title: thing.title,
-        price: thing.price,
-        completed: thing.completed
-      }
-    end
+    things_data = Thing.all.index_by(&:id).transform_values(&:as_json)
 
     render json: {
       things_stored: things_data.to_json,
-      timestamp: Time.now.to_i
+      timestamp: Time.current.to_i
     }
   end
 
   # GET /things/1
   def show
-    render json: @thing
+    render json: { data: @thing }
   end
 
   # POST /things
@@ -28,34 +21,39 @@ class ThingsController < ApplicationController
     @thing = Thing.new(thing_params)
 
     if @thing.save
-      render json: @thing, status: :created, location: @thing
+      render json: { data: @thing }, status: :created, location: @thing
     else
-      render json: @thing.errors, status: :unprocessable_entity
+      render json: { errors: @thing.errors }, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /things/1
   def update
     if @thing.update(thing_params)
-      render json: @thing
+      render json: { data: @thing }
     else
-      render json: @thing.errors, status: :unprocessable_entity
+      render json: { errors: @thing.errors }, status: :unprocessable_entity
     end
   end
 
   # DELETE /things/1
   def destroy
-    @thing.destroy!
+    if @thing.destroy
+      head :no_content
+    else
+      render json: { errors: @thing.errors }, status: :unprocessable_entity
+    end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions
-    def set_thing
-      @thing = Thing.find(params.expect(:id))
-    end
 
-    # Only allow a list of trusted parameters through
-    def thing_params
-      params.expect(thing: [ :title, :price, :completed ])
-    end
+  def set_thing
+    @thing = Thing.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { errors: [ "Thing not found" ] }, status: :not_found
+  end
+
+  def thing_params
+    params.require(:thing).permit(:title, :price, :completed)
+  end
 end
--- app/models/thing.rb
+++ app/models/thing.rb
@@ -1,2 +1,8 @@
 class Thing < ApplicationRecord
+  validates :title, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+
+  def as_json(options = {})
+    super(options.merge(only: [ :id, :title, :price, :completed ]))
+  end
 end
